8a13dc3804f2dda7cd7a9ed9beeaa3986027d6e7
AddingMachine <nick.mohoric@gmail.com>
2011-03-29 21:58:41 -0400

    Showing ifReady stat

--- src/MainClient.hs
+++ src/MainClient.hs
@@ -9,34 +9,35 @@ import Control.Monad.Maybe
 import Control.Monad.Trans (lift)
 import Data.Char (chr)
 import Foreign.C.Types (CChar)
+import Data.Time
+import Data.Time.Clock.POSIX
+
 --import Control.Concurrent.STM
-import System.Console.Haskeline
-import System.Console.Haskeline.IO
+--import System.Console.Haskeline
+--import System.Console.Haskeline.IO
 import Client
 
-ifReady :: Handle -> (Handle -> IO Char) -> IO (Maybe Char)
-ifReady hnd = do
-   ready <- hReady hnd
-   if ready then fmap Just (getHiddenChar) else return Nothing
-
-inputChar :: String -> MaybeT (InputT IO) Char
-inputChar prompt = do
-   m <- lift $ getInputChar prompt
-   case m of
-      Just x -> return x
-      Nothing -> mzero
+ifReady :: Handle -> IO Char -> IO (Maybe Char)
+ifReady hnd io = do
+   ready <- hWaitForInput hnd 100
+   setCursorPosition 20 0
+   putStr $ show ready
+   if ready then fmap Just (io) else return Nothing
 
 moveAbout :: Int -> Int -> GameClientHandle -> String -> IO ()
 moveAbout pY pX gc username = do
  threadDelay 50000
- c <- ifReady stdin
+ c <- ifReady stdin getHiddenChar
  case c of
-    'w' -> updatePlayer pY pX (pY - 1) pX gc username
-    's' -> updatePlayer pY pX (pY + 1) pX gc username 
-    'a' -> updatePlayer pY pX pY (pX - 1) gc username
-    'd' -> updatePlayer pY pX pY (pX + 1) gc username 
-    'p' -> return ()
+    Just 'w' -> updatePlayer pY pX (pY - 1) pX gc username
+    Just 's' -> updatePlayer pY pX (pY + 1) pX gc username 
+    Just 'a' -> updatePlayer pY pX pY (pX - 1) gc username
+    Just 'd' -> updatePlayer pY pX pY (pX + 1) gc username 
+    Just 'p' -> return ()
     _ -> moveAbout pY pX gc username
+
+grabTime :: IO String
+grabTime = fmap show getCurrentTime
 
 getHiddenChar = liftM (chr.fromEnum) c_getch
 foreign import ccall unsafe "conio.h getch"
@@ -86,7 +87,7 @@ main = forever $ do
     initScreen
     hideCursor
     hSetBuffering stdin NoBuffering
-    forkIO $ moveAbout 5 5 gc userName
+    forkIO $ updatePlayer 5 5 5 5 gc userName
     readServer gc userName
 
 initScreen :: IO ()
